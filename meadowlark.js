74152e9e90c6dde655dc51d1b44618f24dde6eb4
Fedyashev <fedjashev@gmail.com>
2017-05-05 12:44:20 +0300

    Add views and layouts. Change routes.

--- meadowlark.js
+++ meadowlark.js
@@ -2,16 +2,23 @@ var express = require("express");
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require("express3-handlebars").create({defaultLayout: "main"});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function(req, res) {
-  res.type("text/plain");
-  res.send("Meadowlark travel");
+  // res.type("text/plain");
+  // res.send("Meadowlark travel");
+  res.render("home");
 });
 
-app.get("/about*", function(req, res) {
-  res.type("text/plain");
-  res.send("About Meadowlark Travel");
+app.get("/about", function(req, res) {
+  // res.type("text/plain");
+  // res.send("About Meadowlark Travel");
+  res.render("about");
 });
 
 app.get("/about/contacts", function(req, res) {
@@ -21,17 +28,19 @@ app.get("/about/contacts", function(req, res) {
 
 // custom 404 page
 app.use(function(req, res, next) {
-  res.type("text/plain");
+  // res.type("text/plain");
   res.status(404);
-  res.send("404 - not found");
+  // res.send("404 - not found");
+  res.render("404");
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type("text/plain");
+  // res.type("text/plain");
   res.status(500);
-  res.send("500 - Server Error");
+  // res.send("500 - Server Error");
+  res.render("500");
 });
 
 app.listen(app.get("port"), function() {

0d6a8373ced91d1a78fe6593d47611bd8fd87912
Fedyashev <fedjashev@gmail.com>
2017-05-12 16:37:19 +0300

    next

--- meadowlark.js
+++ meadowlark.js
@@ -20,8 +20,20 @@ app.use(function(req, res, next) {
   next();
 });
 
+// app.disable("x-powered-by");
+
+// routes
 app.get("/", function(req, res) {
   res.render("home");
+});
+
+app.get("/header", function(req, res) {
+  res.set("Content-Type", "text/plain");
+  var s = "";
+  for (var name in req.headers) {
+    s += name + ": " + req.headers[name] + "\n";
+  }
+  res.send(s);
 });
 
 app.get("/about", function(req, res) {
